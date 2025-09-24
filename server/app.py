2e3c13cf093eef3a29ae75a49ed249204a5a3290
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-09-25 00:12:58 +0300

    likes endpoint

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask import Flask
@@ -8,6 +7,7 @@ from models import db
 from routes.users import UserResource
 from routes.posts import PostResource
 from routes.comments import CommentResource
+from routes.likes import LikeResource
 
 
 
@@ -24,6 +24,7 @@ CORS(app)
 api.add_resource(UserResource, "/users", "/users/<int:user_id>")
 api.add_resource(PostResource, "/posts", "/posts/<int:post_id>")
 api.add_resource(CommentResource, "/comments", "/comments/<int:comment_id>")
+api.add_resource(LikeResource, "/likes", "/likes/<int:like_id>")
 
 
 if __name__ == "__main__":
