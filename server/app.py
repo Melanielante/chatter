c0a798ed39cd6013d1a15859877b31562f4f3f50
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-09-24 23:56:37 +0300

    users crud and routing

--- server/app.py
+++ server/app.py
@@ -2,7 +2,10 @@ from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask import Flask
+from flask_restful import Api
 from models import db
+from routes.users import UserResource
 
 # db = SQLAlchemy()
 # migrate = Migrate()
@@ -12,13 +15,13 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chatter.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
+api = Api(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 CORS(app)
 
 # import routes here later
-
+api.add_resource(UserResource, "/users", "/users/<int:user_id>")
 
 
 
