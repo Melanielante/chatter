a4b50f9488f3832ca09beaf51b91626750ba3588
Joshua Karanja <jokaranja26@gmail.com>
2025-09-25 10:03:29 +0300

    rename routes to auth_routes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from models import db
-from routes.auth_routes import auth_bp
+from auth_routes import auth_bp
 
 # db = SQLAlchemy()
 # migrate = Migrate()
