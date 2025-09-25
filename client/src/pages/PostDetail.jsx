a48a34a5d5ef6c1fa22bc4342cc4b6faf89f619f
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-09-25 13:22:10 +0300

    post detail routing

--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 
 function PostDetail() {
   const { id } = useParams(); // grab post id from URL
